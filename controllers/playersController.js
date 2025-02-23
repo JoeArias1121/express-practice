873eb71edb55404c8f1cf15224581f94dbefbfd1
joearias1121 <joearias1121@gmail.com>
2025-02-23 18:29:35 -0500

    following an mvc model

---
+++ controllers/playersController.js
@@ -0,0 +1,48 @@
+import players from "../model/players.json" assert { type: "json" };
+
+let data = [...players];
+
+export const getAllPlayers = (req, res) => {
+  res.json(data);
+};
+
+export const getPlayer = (req, res) => {
+  const player = data.find((player) => player.id === Number(req.params.id));
+  if (!player) {
+    res.status(404).json({ message: "Player not found" });
+  }
+  res.json(player);
+};
+
+export const addPlayer = (req, res) => {
+  console.log(`Data before operation: ${print(data)}`);
+  data.push(req.body);
+  console.log(`Data after operation: ${print(data)}`);
+  res.json(data);
+};
+
+export const updatePlayer = (req, res) => {
+  console.log(`Data before operation: ${print(data)}`);
+  data = data.map((player) => {
+    if (player.id === req.body.id) {
+      player.firstName = req.body.firstName;
+      player.lastName = req.body.lastName;
+    }
+    return player;
+  });
+  console.log(`Data after operation: ${print(data)}`);
+  res.json(data);
+};
+
+export const deletePlayer = (req, res) => {
+  console.log(`Data before operation: ${print(data)}`);
+  data = data.filter((player) => player.id !== req.body.id);
+  console.log(`Data after operation: ${print(data)}`);
+  res.json(data);
+};
+
+function print(data) {
+  data.forEach((player) => {
+    console.log(player);
+  });
+}
--- routes/api/players.js
+++ routes/api/players.js
@@ -1,47 +1,16 @@
 import express from "express";
-//import path from "node:path"
-//import { fileURLToPath } from "node:url";
-import players from "../../data/players.json" assert { type: "json" };
+import * as playersController from "../../controllers/playersController.js";
+import players from "../../model/players.json" assert { type: "json" };
 
-//const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const router = express.Router();
 let data = [...players];
 
-function print(data) {
-  data.forEach((player) => {
-    console.log(player);
-  });
-}
+const router = express.Router();
 
-router
-  .route("/")
-  .get((req, res) => {
-    res.json(data);
-  })
-  .post((req, res) => {
-    console.log(`Data before operation: ${print(data)}`);
-    data.push(req.body);
-    console.log(`Data after operation: ${print(data)}`);
-    res.json(data);
-  })
-  .put((req, res) => {
-    console.log(`Data before operation: ${print(data)}`);
-    data = data.map((player) => {
-      if (player.id === req.body.id) {
-        player.firstName = req.body.firstName;
-        player.lastName = req.body.lastName;
-      }
-      return player;
-    });
-    console.log(`Data after operation: ${print(data)}`);
-    res.json(data);
-  })
-  .delete((req, res) => {
-    console.log(`Data before operation: ${print(data)}`);
-    data = data.filter((player) => player.id !== req.body.id);
-    console.log(`Data after operation: ${print(data)}`);
-    res.json(data);
-  });
+router.route("/")
+  .get(playersController.getAllPlayers)
+  .post(playersController.addPlayer)
+  .put(playersController.updatePlayer)
+  .delete(playersController.deletePlayer);
 
 router.route("/:id").get((req, res) => {
   const player = data.find((player) => player.id === Number(req.params.id));
